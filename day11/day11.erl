d413132e01bd5c00530b673b880dd40e96d00dc6
John Daily <git@epep.us>
2019-12-14 21:47:36 -0500

    Day 11 part 2!
    Very pleased with how little I had to change the code supporting part
    1 to work with both parts. The sparse matrix worked flawlessly.

--- day11/day11.erl
+++ day11/day11.erl
@@ -10,7 +10,58 @@ part1(Filename) ->
     Robot = intcode:load(Filename),
     Surface = matrix:initialize(black),
     Start = {500, 500, up}, %% X, Y, Direction
-    run_robot(Robot, Start, matrix:get_value(coords(Start), Surface), sets:new()).
+    {PaintedSet, _PaintedSurface} =
+        run_robot(Robot, Start, matrix:get_value(coords(Start), Surface),
+                  sets:new()),
+    sets:size(PaintedSet).
+
+part2(Filename) ->
+    Robot = intcode:load(Filename),
+    Start = {500, 500, up}, %% X, Y, Direction
+    Surface = matrix:initialize(black),
+    %% I could modify Surface to paint the starting point white, but
+    %% instead I'll just lie about the camera input. The robot is
+    %% going to paint the starting point regardless.
+    {PaintedSet, PaintedSurface} =
+        run_robot(Robot, Start, {white, Surface}, sets:new()),
+    print(PaintedSet, PaintedSurface).
+
+to_char(white) ->
+    64;
+to_char(black) ->
+    32.
+
+%% We will obtain upper and lower bounds on X and Y by analyzing
+%% PaintedSet. Alternatively we could try some sparse folding trickery
+%% on arrays, but that gets hairy.
+print(PaintedSet, PaintedSurface) ->
+    {{UpperX, LowerX}, {UpperY, LowerY}} = set_boundaries(PaintedSet),
+    lists:foreach(fun(Row) ->
+                          lists:foreach(fun(Col) ->
+                                                {Color, _} = matrix:get_value({Col, Row}, PaintedSurface),
+                                                io:format("~c", [to_char(Color)])
+                                        end, lists:seq(LowerX, UpperX)),
+                          io:format("~n")
+                  end, lists:reverse(lists:seq(LowerY, UpperY))).
+
+set_boundaries(Set) ->
+    sets:fold(fun(Coord, Bounds) ->
+                      compare_bounds(Coord, Bounds)
+              end, {{0, 50000}, {0, 50000}}, Set).
+
+%% Technically this is inadequate if the fold function is pathological
+%% and/or the number of values is too small; e.g.:
+%%
+%% > day11:compare_bounds({5, 8}, {{0, 500}, {0, 500}}).
+%% {{5,500},{8,500}}
+compare_bounds({X, Y}, {XBounds, YBounds}) ->
+    {compare_bounds(X, XBounds), compare_bounds(Y, YBounds)};
+compare_bounds(I, {Upper, Lower}) when I > Upper ->
+    {I, Lower};
+compare_bounds(I, {Upper, Lower}) when I < Lower ->
+    {Upper, I};
+compare_bounds(_, Bounds) ->
+    Bounds.
 
 color_int(black) ->
     0;
@@ -74,8 +125,12 @@ run_robot(Robot, CurSpot, {CurColor, Surface}, PaintedSet) ->
     Coords = coords(CurSpot),
     NewPaintedSet = sets:add_element(Coords, PaintedSet),
     case camera_input(Robot, CurColor) of
-        {terminated, _State} ->
-            sets:size(NewPaintedSet);
+        {terminated, NewRobot} ->
+            {[PaintColor, _Direction], _NewR} =
+                intcode:extract_outputs(NewRobot),
+            NewSurface =
+                matrix:set_value(Coords, color_atom(PaintColor), Surface),
+            {NewPaintedSet, NewSurface};
         {blocked, NewRobot} ->
             {[PaintColor, Direction], NewNewRobot} =
                 intcode:extract_outputs(NewRobot),
